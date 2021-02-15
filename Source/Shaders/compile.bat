b35019f019cc103d1a6400f8297402baee72f0a3
Han Shu Bin <HordrenHan@outlook.com>
2021-02-16 05:21:35 +0800

    [Add] Added VulkanWindowBase Shaders

---
+++ Source/Shaders/compile.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.2.162.1/Bin32/glslc.exe VulkanWindowBase.vert -o VulkanWindowBaseVert.spv
+C:/VulkanSDK/1.2.162.1/Bin32/glslc.exe VulkanWindowBase.frag -o VulkanWindowBaseFrag.spv
+pause
